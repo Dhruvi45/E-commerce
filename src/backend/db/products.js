9dadd42f8d47fada8f9faeea987e6eca359cdca9
SYNOVERGETECH\joshidhr <dhruvi.joshi@synoverge.com>
2023-06-22 15:14:28 +0530

    single product related changes

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -11,39 +11,39 @@ export const products = [
     title: "You Can WIN",
     author: "Shiv Khera",
     price: "5000",
-    Language : 'English',
-    rating:4.9
+    Language: 'English',
+    rating: 4
   },
   {
     _id: uuid(),
     title: "Rich Dad Poor Dad",
     author: "Robert T. Kiyosaki",
     price: "298",
-    Language : 'English',
-    rating:4.6
+    Language: 'English',
+    rating: 3
   },
   {
     _id: uuid(),
     title: "Think and Grow Rich",
     author: "Napoleon Hill",
     price: "1350",
-    Language : 'English',
-    rating:4.4
+    Language: 'English',
+    rating: 5
   },
   {
     _id: uuid(),
     title: "The Silent Patient",
     author: "Alex Michaelides",
     price: "2121",
-    Language : 'English',
-    rating:4.5
+    Language: 'English',
+    rating: 2
   },
   {
     _id: uuid(),
     title: "The Silent Patient",
     author: "Alex Michaelides",
     price: "2121",
-    Language : 'English',
-    rating:4.5
+    Language: 'English',
+    rating: 1
   },
 ];
--- src/frontend/pages/product/product.js
+++ src/frontend/pages/product/product.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { axiosGet } from '../../utils/Helper'
 import { toast } from 'react-toastify';
 import { useParams } from 'react-router-dom';
-import { Loader } from 'rsuite';
+import { Loader, Rate } from 'rsuite';
 import "../../assets/style/product.css"
 export default function Product() {
   const { id } = useParams()
-  const [product, setProduct] = useState([])
+  const [product, setProduct] = useState()
   const [loading, setLoading] = useState(false)
   const getProduct = async () => {
+    setLoading(true)
     await axiosGet(`/api/products/${id}`)
       .then((res) => {
         console.log('res', res)
@@ -25,17 +26,20 @@ export default function Product() {
 
   }, [])
   return (
-    <>{loading ? <Loader size="lg" /> :
+    <>{loading && product === undefined ? <Loader size="lg" /> :
       <>
-        <div>
-          <img src={require(`../../assets/images/${product.title}.jpg`)} alt={`${product.title}`} />
-        </div>
-        <div>
-          <h1>{product.title}</h1>
-          <p><b>Prise: </b>{product.price}</p>
-          <p><b>Author: </b>{product.author}</p>
-          <p><b>Language: </b>{product.Language}</p>
-          <p><b>Rating: </b>{product.rating}</p>
+        <div className='d-flex'>
+          <div>
+            {product?.title && <img src={require(`../../assets/images/${product?.title}.jpg`)} alt={`${product?.title}`} />}
+          </div>
+          <div className='productDetails'>
+            <h1>{product?.title}</h1>
+            <Rate value={product?.rating} />
+            <p><b>Prise: </b>{product?.price}</p>
+            <p><b>Author: </b>{product?.author}</p>
+            <p><b>Language: </b>{product?.Language}</p>
+            <p><b>Rating: </b>{product?.rating}</p>
+          </div>
         </div>
       </>
     }
